93e2b82a3c7a7d367a6d8a4ff4bd9c786b47db57
AnandaVenkataRaman <alanandvr@gmail.com>
2025-08-26 20:35:39 +0800

    feat: add for test demo

--- src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
+++ src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
@@ -6,6 +6,7 @@ package com.sddevops.jenkins_maven.eclipse;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Test");
+        System.out.println("Test Demo");
+        
     }
 }
